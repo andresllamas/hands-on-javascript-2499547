0b7e030a21ff29a296b15971e66046ecf38072ae
Jonathan Andrés <andresllamas.7@gmail.com>
2024-08-21 05:40:54 +0000

    Aug 20 changes

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 export default {
   // Get API key from https://home.openweathermap.org/users/sign_up
-  appid: [OpenWeatherAPIkey],
+  appid: "57125c3767210d8e355218a38f778157",
   units: "metric",
-  location: "carpinteria,ca,us",
+  location: "guadalajara,mx",
 };
